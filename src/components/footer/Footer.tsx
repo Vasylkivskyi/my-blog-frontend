2ff9e58985302f4a0079115ec69581968a817543
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2019-12-14 08:06:36 +0200

    add social lints

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./footer.scss";
+import facebook from "../../images/facebook.svg";
+import twitter from "../../images/twitter.svg";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <a href="https://twitter.com/PVasylkivskyi">
-        Follow me on twitter @PVasylkivskyi
+      Follow Me
+      <a className="social-links" href="https://twitter.com/PVasylkivskyi">
+        <img className="social" src={twitter} alt="twitter icon" />
+      </a>
+      <a
+        className="social-links"
+        href="https://www.facebook.com/pasha.vasylkivskiy"
+      >
+        <img className="social" src={facebook} alt="facebook icon" />
       </a>
     </footer>
   );
