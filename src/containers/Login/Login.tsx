d6d7a4b3c795354ba601fcd92e8750ed369a9001
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2019-12-30 07:23:39 +0200

    make modal to work

--- src/containers/Login/Login.tsx
+++ src/containers/Login/Login.tsx
@@ -1,44 +1,50 @@
-import React from 'react';
+import * as React from 'react';
 import './login.scss';
 import { FaGoogle, FaFacebook } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
 import Modal from '../../components/modal/Modal';
 
-
-const Login = () => (
-  <div className="login">
+const Login: React.FC = () => {
+  const [isShowing, setShowing] = React.useState<boolean>(false);
+  const toggleModal = () => {
+    console.log('😈');
+    setShowing(!isShowing);
+  };
+  return (
+    <div className="login">
 To leave a comment please
-    {' '}
-    <span className="login-button">login</span>
+      {' '}
+      <span className="login-button" onClick={() => toggleModal()} tabIndex={0} role="button">login</span>
 .
-    {' '}
-    {' '}
+      {' '}
+      {' '}
         Don&apos;t have an account?
-    {' '}
-    <Link to="/register">
-      <span className="login-button">Register</span>
-    </Link>
-    <Modal isShoving={false} toggleModal={() => console.log('hello')}>
-      <div className="login-modal">
-        <div className="login-card">
-          <h3 className="login-header">Log in and leave a comment</h3>
-          <div className="google btn">
-            <FaGoogle className="icon" />
+      {' '}
+      <Link to="/register">
+        <span className="login-button">Register</span>
+      </Link>
+      <Modal isShoving={isShowing} toggleModal={() => toggleModal()}>
+        <div className="login-modal">
+          <div className="login-card">
+            <h3 className="login-header">Log in and leave a comment</h3>
+            <div className="google btn">
+              <FaGoogle className="icon" />
 Log In with Google
-          </div>
-          <div className="google btn">
-            <FaFacebook className="icon" />
+            </div>
+            <div className="google btn">
+              <FaFacebook className="icon" />
 Log In with Facebook
-          </div>
+            </div>
           Don&apos;t have an account?
-          {' '}
-          <Link to="/register">
-            <span className="login-button">Register</span>
-          </Link>
+            {' '}
+            <Link to="/register">
+              <span className="login-button">Register</span>
+            </Link>
+          </div>
         </div>
-      </div>
-    </Modal>
-  </div>
-);
+      </Modal>
+    </div>
+  );
+};
 
 export default Login;
