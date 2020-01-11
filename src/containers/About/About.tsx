5f35fd0f07a65e2b42726c3ba392449ebdc10615
Pavel Vasylkivskiy <pavel.vasylkivskiy@gmail.com>
2020-01-11 08:49:36 +0200

    replace image to bottom of the page

--- src/containers/About/About.tsx
+++ src/containers/About/About.tsx
@@ -8,7 +8,7 @@ const About: React.FC = () => (
   <div className="new-post-container">
     <h1>About</h1>
     <div className="content">
-      <img src={Flow} alt="The reaver flow" />
+
       <p>Welcome to &rdquo;Inside the flow&rdquo;. My name is Pavlo. I&apos;m the author of this blog.</p>
       <p>An idea to create the blog came to me one day when I had a walk to my job. I&apos;m always walk to work for about one hour. And some times in my head born thoughts. Every time it happens I want to write it down.</p>
       <p>I&apos;m a web developer and to become a professional I need to constantly improve my programming skills and I decided to create this project. Simultaneously, I will practice writing code and will have the opportunity to save my thoughts.</p>
@@ -16,6 +16,7 @@ const About: React.FC = () => (
       <p>
 The title &rdquo;Inside the flow&rdquo; means for me next. When people start to think about something they fall into the whirlpool of different thoughts and sometimes I catch myself on the thought that I&apos;m in a state of something like a meditation. Psychologist Mihaly Csikszentmihalyi&apos;s called thi state as a flow. And in my blog I&apos;ll share my thoughts that appearing from inside this state. I hope you will enjoy it.
       </p>
+      <img src={Flow} alt="The reaver flow" />
     </div>
   </div>
 );
